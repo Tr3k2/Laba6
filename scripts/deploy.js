e3a59a720b7e0e892be74fa228389a062079405c
Андрей Киселев <elsolnce2019@gmail.com>
2026-04-08 01:59:28 +0500

    Add all project files

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  
+  await token.waitForDeployment();
+  
+  console.log("Token deployed to:", await token.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
